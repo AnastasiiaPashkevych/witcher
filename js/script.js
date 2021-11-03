6fb5a0fb5d069a4cea95d14ec228e795b0ab5e0d
Anastasia <nastyapashkevich10@gmail.com>
2021-11-03 21:15:04 +0100

    feat: add adaptive

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 $(function () {
 
+  // Menu
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active')
+  })
+
   // Video
   $('[data-fancybox]').fancybox({
     youtube: {}
@@ -11,7 +16,15 @@ $(function () {
     slidesToScroll: 1,
     asNavFor: '.heroes__slider-text',
     prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
-    nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>'
+    nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>',
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
   });
   $('.heroes__slider-text').slick({
     slidesToShow: 1,
